ed82af7b27513a2d44dac3cc0eedfd43896c3309
Abdul Rashid O'Meni <AO411@canterbury.ac.uk>
2022-09-30 20:48:29 +0100

    Add project files.

---
+++ GuessingGame_SecretWord/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Creating guess game
+
+//declaring and initialiszing variables
+string secrectWord = "kahakmed";
+string guess = "";
+int userGuessCount = 0;
+int userGuessLimit = 3;
+bool isOutOfGuess = false;
+
+while(guess != secrectWord && !isOutOfGuess)
+{
+    if (userGuessCount < userGuessLimit)
+    {
+        Console.WriteLine("Please enter a guess word: ");
+        guess = Console.ReadLine();
+        userGuessCount++;
+    }
+    else 
+    { 
+        isOutOfGuess = true;
+    }
+}
+if (isOutOfGuess)
+{
+    Console.WriteLine("You lose!");
+}
+else
+{
+    Console.WriteLine("You Win");
+
+}
+
+
+
